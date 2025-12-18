2e06b074e6f9d7e05ac4e2eea320794b53477b9b
chacon5037 <edisonchacon503@gmail.com>
2025-12-18 11:24:14 -0500

    Js Script adding based on rubric

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('Hello!');
+
+const p = document.createElement("p");
+p.textContent = "Thanks for visiting!";
+document.body.appendChild(p);
+
+const popularColors = ["Black", "White", "Navy", "Olive"];
+
+popularColors.forEach(color => {
+  console.log("Popular color:", color);
+});
+
+const message = document.createElement("p");
+message.textContent = "Explore popular styles and color combos";
+document.body.appendChild(message);
